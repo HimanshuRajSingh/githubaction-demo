d8f5884ad072fc1241a2cc04e566efa488676574
Himanshu Raj Singh <60730117+HimanshuRajSingh@users.noreply.github.com>
2025-03-28 13:16:24 +0530

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,8 +1,8 @@
 # test_example.py
-def test_addition():
-    assert 1 + 1 == 2
+# def test_addition():
+#     assert 1 + 1 == 2
 
-def test_string():
-    a = "hello"
-    b = "hello"
-    assert a == b
+# def test_string():
+#     a = "hello"
+#     b = "hello"
+#     assert a == b
